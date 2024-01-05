f2f8870ed697b62b7a56efbf0ec5aebfc88b6193
Omar-Driouch <omardriouch29@gmail.com>
2024-01-05 09:30:46 +0100

    update

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,14 +6,14 @@ const Header = () => {
       <header
         style={{
           height: "90px",
-          background: "#ff0000",
+          background: "#fff",
           padding: "0 30px",
           display: "flex",
           alignItems: "center",
           boxShadow: "0 5px 15px rgba(0,0,0,0.2)",
         }}
       >
-        <h1 style={{ color: "#fff" }}>Gallery</h1>
+        <h1 style={{ color: "#000" }}>Gallery</h1>
       </header>
     </>
   );
